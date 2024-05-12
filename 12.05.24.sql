fe8e43bb913a12788533ba6452df8c93221eedbc
JENARIKIT <167493856+JENARIKIT@users.noreply.github.com>
2024-05-12 22:23:17 +0700

    Add files via upload

---
+++ 12.05.24.sql
@@ -0,0 +1,16 @@
+--IN 2023 USE TRADE
+
+CREATE VIEW IN_2023_USE_TRADE
+AS
+
+SELECT 
+	T.Trade,
+	SUM(FG.FGQuatity/1000.0)*-1 AS QUANTITY,
+	strftime('%Y-%m',P.start_date) AS MonthID
+FROM ProcessOrder as P,Material AS M,FG,Trade As T
+WHERE M.Material = P.Material_ID
+AND P.ProcessOrder = FG.RowLabels
+AND T.Material = P.Material_ID
+AND MonthID like '2023%'
+Group by T.Trade,MonthID
+Order by MonthID;
